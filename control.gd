474b651c0c4c0cf9ed24312b2d5f1c41515b9b48
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2024-03-24 21:51:51 +0700

    some stuff

--- control.gd
+++ control.gd
@@ -2,7 +2,7 @@ extends Node
 
 @onready var parent = get_node("..")
 @onready var label = parent.get_node("HUD/pilot")
-
+var exited = 0
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
@@ -16,7 +16,9 @@ func _input(event):
 func _process(_delta):
 	if Input.is_action_just_pressed("use"):
 		Global.piloting = not Global.piloting
-	
+	if Input.is_action_just_pressed("exit") and not exited:
+		exited=1
+		parent.get_node("submarine/player").reparent($"../map")
 	if Global.piloting:
 		label.text = "is piloting"
 	else:
